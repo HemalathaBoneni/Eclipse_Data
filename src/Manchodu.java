435b41c0bd1e12688c111487dc51a01c48756ad4
hemab <hema.boneni@gmail.com>
2024-07-27 23:04:18 +0530

    Adding files

---
+++ src/Manchodu.java
@@ -0,0 +1,13 @@
+
+public class Manchodu
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(new Manchodu().hema());
+ 	}
+    String hema()
+    {
+	   return "bramar";
+    }
+}
